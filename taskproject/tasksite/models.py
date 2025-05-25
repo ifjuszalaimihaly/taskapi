1ea4d2904f7aed0c2c67ff29bdb7305ad141a6b8
ifjuszalaimihaly <ifjuszalaimihaly@gmail.com>
2025-05-25 17:51:54 +0200

    created models migrations and serializers

--- taskproject/tasksite/models.py
+++ taskproject/tasksite/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        IN_PROGRESS = 'in_progress', 'In Progress'
+        COMPLETED = 'completed', 'Completed'
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+
+    def __str__(self):
+        return self.title
