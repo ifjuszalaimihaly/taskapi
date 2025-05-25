6fcec3260ebe43d192ecd06dfde3a3d9ef964af5
ifjuszalaimihaly <ifjuszalaimihaly@gmail.com>
2025-05-25 17:56:14 +0200

    added create tasks functionality

--- taskproject/tasksite/views.py
+++ taskproject/tasksite/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework import serializers, status
 
-# Create your views here.
+@api_view(['GET'])
+def ApiOverview(request):
+    api_urls = {
+        'all_items': '/',
+        'Search by Category': '/?category=category_name',
+        'Search by Subcategory': '/?subcategory=category_name',
+        'Add': '/create',
+        'Update': '/update/pk',
+        'Delete': '/item/pk/delete'
+    }
+
+    return Response(api_urls)
+
+@api_view(['POST'])
+def add_items(request):
+    item = TaskSerializer(data=request.data)
+
+    # validating for already existing data
+    if Task.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data already exists')
+
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
